cb53f54e25c185ee11ce0cc101ae7f9750ad22ce
Mikael Brevik <mikaelbre@gmail.com>
2023-06-08 15:11:16 +0200

    fix: barcode valid tests

--- tools/types.ts
+++ tools/types.ts
@@ -1,4 +1,4 @@
 
-export type TopicName = 'sensors/location';
-export const availableTopics = ["sensors/location"] as readonly TopicName[];
-export const topicSet = {"sensors/location":"sensors/location/location.schema.json"} satisfies Record<TopicName, string>;
+export type TopicName = 'sensors/location' | 'validators/barcode';
+export const availableTopics = ["sensors/location","validators/barcode"] as readonly TopicName[];
+export const topicSet = {"sensors/location":"sensors/location/location.schema.json","validators/barcode":"validators/barcode/barcode.schema.json"} satisfies Record<TopicName, string>;
