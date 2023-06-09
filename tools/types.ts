5ef1fceca1e88708a667d2500de9ba411c5e7913
Mikael Brevik <mikaelbre@gmail.com>
2023-06-09 14:46:31 +0200

    spec: propose specification /validators/[deviceId]/response

--- tools/types.ts
+++ tools/types.ts
@@ -1,4 +1,4 @@
 
-export type TopicName = 'sensors/location' | 'validators/barcode';
-export const availableTopics = ["sensors/location","validators/barcode"] as readonly TopicName[];
-export const topicSet = {"sensors/location":"sensors/location/location.schema.json","validators/barcode":"validators/barcode/barcode.schema.json"} satisfies Record<TopicName, string>;
+export type TopicName = 'sensors/location' | 'validators/barcode' | 'validators/[deviceId]/response';
+export const availableTopics = ["sensors/location","validators/barcode","validators/[deviceId]/response"] as readonly TopicName[];
+export const topicSet = {"sensors/location":"sensors/location/location.schema.json","validators/barcode":"validators/barcode/barcode.schema.json","validators/[deviceId]/response":"validators/[deviceId]/response/response.schema.json"} satisfies Record<TopicName, string>;
