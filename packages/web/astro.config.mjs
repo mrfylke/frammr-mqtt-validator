42d677656b962bf5b239c51de5caf8bae705f256
Mikael Brevik <mikaelbre@gmail.com>
2023-07-07 16:09:32 +0200

    chore: fix spec routing

--- packages/web/astro.config.mjs
+++ packages/web/astro.config.mjs
@@ -7,10 +7,7 @@ import tailwind from "@astrojs/tailwind";
 export default defineConfig({
   integrations: [tailwind()],
 
-  site:
-    process.env.NODE_ENV === "production"
-      ? "https://mrfylke.github.io/hwb-standard/"
-      : undefined,
+  base: "/hwb-standard",
 
   markdown: {
     remarkPlugins: [remarkMermaid],
