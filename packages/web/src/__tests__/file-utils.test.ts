592413a1feb64de51ea74028e1fa7ca1dc29d143
Mikael Brevik <mikaelbre@gmail.com>
2023-08-02 14:05:50 +0200

    chore: adds topic tree to web

---
+++ packages/web/src/__tests__/file-utils.test.ts
@@ -0,0 +1,78 @@
+import { test, expect } from "vitest";
+import {
+  createTopicTree,
+  printMarkdownTree,
+  type TopicNode,
+} from "../file-utils";
+
+test("file-utils", () => {
+  const tree = createTopicTree([
+    "specifications/a/b/c/c.md",
+    "specifications/a/b/b.md",
+    "specifications/a/foo/foo.md",
+    "specifications/a/bar/bar.md",
+    "specifications/foo/bar/bar.md",
+  ]);
+
+  const expected = {
+    url: "",
+    children: {
+      a: {
+        url: "a",
+        children: {
+          b: {
+            url: "a/b/b.md",
+            topic: "a/b",
+            children: {
+              c: {
+                url: "a/b/c/c.md",
+                topic: "a/b/c",
+                children: {},
+              },
+            },
+          },
+          foo: {
+            url: "a/foo/foo.md",
+            topic: "a/foo",
+            children: {},
+          },
+          bar: {
+            url: "a/bar/bar.md",
+            topic: "a/bar",
+            children: {},
+          },
+        },
+      },
+      foo: {
+        url: "foo",
+        children: {
+          bar: {
+            url: "foo/bar/bar.md",
+            topic: "foo/bar",
+            children: {},
+          },
+        },
+      },
+    },
+  } as TopicNode;
+
+  expect(tree).toEqual(expected);
+});
+test("file-utils print", () => {
+  const tree = createTopicTree([
+    "specifications/a/a.md",
+    "specifications/foo/foo.md",
+    "specifications/foo/bar/bar.md",
+    "specifications/foo/baz/baz.md",
+  ]);
+
+  const expected = `/
+├── [a](/topics/a)
+└── [foo](/topics/foo)
+    ├── [bar](/topics/foo/bar)
+    └── [baz](/topics/foo/baz)
+`;
+
+  const result = printMarkdownTree(tree);
+  expect(result).toEqual(expected);
+});
--- packages/web/src/file-utils.ts
+++ packages/web/src/file-utils.ts
@@ -1,12 +1,64 @@
 import type { TopicName } from "@frammr/mqtt-types";
+import { dirname } from "node:path";
 
-type TopicNode = {
+export type TopicNode = {
   url: string;
-  topic?: TopicName;
-  children: TopicNode[];
+  topic?: string;
+  children: {
+    [key: string]: TopicNode;
+  };
 };
 
 export function createTopicTree(filenames: string[]): TopicNode {
-  const topicMap: { [key: string]: TopicNode } = {};
-  return { url: "", children: [] };
+  const root: TopicNode = {
+    url: "",
+    children: {},
+  };
+
+  filenames.forEach((path) => {
+    const relativePath = path.split("specifications/")[1];
+    const parts = relativePath.split("/");
+    let node = root;
+    parts.forEach((part, index) => {
+      if (node.children && !node.children[part]) {
+        if (part.replace(".md", "") === parts[index - 1]) {
+          node.topic = dirname(relativePath) as TopicName;
+          node.url = relativePath;
+        } else {
+          node.children[part] = {
+            url: parts.slice(0, index + 1).join("/"),
+            children: {},
+          };
+        }
+      }
+      node = (node.children?.[part] as TopicNode) ?? {};
+    });
+  });
+
+  return root;
+}
+
+export function printHtmlTree(node: TopicNode, indent = ""): string {
+  if (!node.children && node.topic) {
+    return `${indent}└── <a href="/topic/${val.topic}">${node.topic}</a>\n`;
+  }
+  if (!node.children && !node.topic) {
+    return ``;
+  }
+  let result = node.url === "" ? "/\n" : "";
+  Object.entries(node.children).forEach(([key, val], index, all) => {
+    const isLast = index == all.length - 1;
+    const sign = isLast ? "└──" : "├──";
+    if (val.topic) {
+      result += `${indent}${sign} <a href="/topic/${val.topic}">${key}</a>\n`;
+    } else {
+      result += `${indent}${sign} ${key}\n`;
+    }
+
+    if (val.children) {
+      const prefixSign = isLast ? " " : "│";
+      result += printHtmlTree(val, `${indent}${prefixSign}    `);
+    }
+  });
+  return result;
 }
